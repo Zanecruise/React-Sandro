f52e7faee8da8e2e7e5634f089ea0a505a2d0351
Patrick <48337395+Zanecruise@users.noreply.github.com>
2024-08-15 22:16:35 -0300

    teste

---
+++ Projeto Sandro/site/src/pages/home.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+    return (
+        <>
+            <header>
+                
+                <h1>HOME</h1>
+                <h2>tses</h2>
+                <h3>teste</h3>
+                <ul>
+                    <li><a href="/#">Home</a></li>
+                    <li><a href="/#">Soluções</a></li>
+                    <li><a href="/#">Depoimentos</a></li>
+                    <li><a href="/#">Preços</a></li>
+                    <li><a href="/#">Contato</a></li>
+                </ul>
+            </header>
+        </>
+    )
+}
